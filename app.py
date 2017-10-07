a6bcf42ce385e34da1fad422f4210f9f8e6a9212
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-10-07 17:42:56 +0530

    modify gitignore

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+CONNECTION = 'mongodb://sumedh:block-shop@ds113435.mlab.com:13435/block-shop'
+client = MongoClient(CONNECTION)
+db = client.block-shop
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return 'success', 200
-#
-# @app.route('/makeNode', methods=['GET', 'POST'])
-# def makeNode():
-#     return 'make node', 200
-#
-# @app.route('/')
+
+@app.route('/mine', methods=['GET', 'POST'])
+def mine():
+    return 'mine here', 200
+
+@app.route('/transactions/new', methods=['POSR'])
+def newTransaction():
+    """
+        Saving to mongoDB takes place here.
+    """
+    return 'new transaction', 200
+
+@app.route('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
