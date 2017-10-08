01c6e65df4cdcaf86259c7eae63e8b21b53af8b6
kvnamipara <kvnamipara@gmail.com>
2017-10-08 12:26:18 +0530

    majority wins criteria implemented

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 CONNECTION = 'mongodb://blockshop:blockshop@ds113445.mlab.com:13445/blockshop'
 client = MongoClient(CONNECTION)
 db = client.blockshop
-TRANSACTION_LIMIT = 2
+TRANSACTION_LIMIT = 1
 ORDER_ID = 1000
-REVIEW_REQUIRED = 2
+MIN_REVIEW_REQUIRED = 3
 
 
 
@@ -55,15 +55,19 @@ class block_currency(object):
 
     def distribute_reward(self):
         self.total += self.generated
-        distribution = TRANSACTION_LIMIT * (REVIEW_REQUIRED + 1)
-        individual_addition = float(self.generated) / float(distribution)
-        print individual_addition
+        # # distribution = TRANSACTION_LIMIT * (MIN_REVIEW_REQUIRED + 1)
+        # # individual_addition = float(self.generated) / float(distribution)
+        # print individual_addition
         last_block_id = db.blockchain.count()-1
         print last_block_id
         last_block = db.blockchain.find_one({"index":last_block_id})
+        distribution = 0
+        for ele in last_block['transactions']:
+            distribution += len(ele['pos_review_by'])
+        individual_addition = float(self.generated)/float(distribution)
         print last_block
         for ele in last_block['transactions']:
-            user_list = ele['review_by']
+            user_list = ele['pos_review_by']
             for user_id in user_list:
                 db.user.update({"user_id":int(user_id)},{"$inc":{"currency":individual_addition}})
 
@@ -89,7 +93,7 @@ class currentBlock(object):
         self.last_index = index
 
     def add_transaction(self, trans):
-        self.transactions.append({'order_id':trans.order_id,'seller_id':trans.seller_id,'address':trans.address,'review_by':trans.review_by})
+        self.transactions.append({'order_id':trans.order_id,'seller_id':trans.seller_id,'address':trans.address,'pos_review_by':trans.pos_review_by,'neg_review_by':trans.neg_review_by})
         print self.transactions
         del current_transactions.transaction_dict[trans.order_id]
         if len(self.transactions) == TRANSACTION_LIMIT:
@@ -112,34 +116,46 @@ class Transaction(object):
         self.vehicle_no = vehicle_no
         self.address = address
         self.features = None
-        self.review_by = []
-        self.review_no = 0
+        self.pos_review_by = []
+        self.neg_review_by = []
+        self.positive_response = 0
+        self.negative_response = 0
         self.verified = False
         ORDER_ID += 1
 
     def check_verification(self):
-        if self.review_no == REVIEW_REQUIRED:
-            self.verified = True
-            currentblock.add_transaction(self)
-        else:
-            pass
+        print MIN_REVIEW_REQUIRED
+        if (self.positive_response+self.negative_response) >= MIN_REVIEW_REQUIRED:
+            print "validation done..."
+            if self.positive_response>self.negative_response:
+                self.verified = True
+                currentblock.add_transaction(self)
+            else:
+                self.features = None
+                self.pos_review_by = []
+                self.neg_review_by = []
+                self.positive_response = 0
+                self.negative_response = 0
+                self.verified = False
 
     def review_true(self,user_id):
         # self.review_by.append(user_id)
-        self.review_no += 1
-        self.review_by.append(user_id)
+        self.positive_response += 1
+        self.pos_review_by.append(user_id)
+        print "hello"
         self.check_verification()
+        print "done"
 
-    def review_false(self):
-        self.features = None
-        self.review_no = 0
-        self.review_by = []
+    def review_false(self,user_id):
+        self.negative_response += 1
+        self.neg_review_by.append(user_id)
+        self.check_verification()
 
     def add_feature(self,features):
         self.features = features
 
     def __str__(self):
-        return str({})
+        return str([self.order_id,self.seller_id,self.positive_response,self.negative_response])
 
 
 # global vars
@@ -170,7 +186,9 @@ def review():
         feature_user_id = request.form['user_id']
         features = {'ABS':request.form['abs'],'power_steering':request.form['powersteering'], 'power_window': request.form['powerwindow'], 'ac': request.form['ac']}
         current_transactions.transaction_dict[int(order_id)].features = features
-        current_transactions.transaction_dict[int(order_id)].review_by.append(feature_user_id)
+        current_transactions.transaction_dict[int(order_id)].pos_review_by.append(feature_user_id)
+        current_transactions.transaction_dict[int(order_id)].positive_response += 1
+        current_transactions.transaction_dict[int(order_id)].check_verification()
         return redirect(url_for('index'))
 
 @app.route('/review/type',methods=['POST'])
@@ -181,7 +199,7 @@ def type():
         if str(request.form['status'])=='1':
             current_transactions.transaction_dict[int(order_id)].review_true(user_id)
         else:
-            current_transactions.transaction_dict[int(order_id)].review_false()
+            current_transactions.transaction_dict[int(order_id)].review_false(user_id)
     return redirect(url_for('index'))
 
 
